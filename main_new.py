43ef241f22fb7c7b773635cf6a5a7148fb81990b
kyle <634405768@qq.com>
2023-02-15 22:10:41 +0800

    1.rewrite all python code in main_new.py 2.using INI file as config file rather than txt file 3.add timer to auto save text(10s)

--- main_new.py
+++ main_new.py
@@ -4,116 +4,101 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 import qdarkstyle
 
-
 # 枚举左上右下以及四个定点
 Left, Top, Right, Bottom, LeftTop, RightTop, LeftBottom, RightBottom = range(8)
- 
-class App(QWidget):
-    # TODO: 当最大化后，双击标题栏要窗口化
-    # 四周边距
-    Margins = 2
-    left_mem = 1277
-    top_mem = 265
-    # print(self.left_init)
-    # print(self.top_init)
-    width_mem = 300
-    height_mem = 335
 
+class App(QWidget):
+    
     def __init__(self):
         super().__init__()
-        self.str = []
-        self.settings = QtCore.QSettings("config.ini",QtCore.QSettings.IniFormat)
-        print(self.settings.fileName())
-        pos= self.settings.value("pos", QtCore.QVariant(QtCore.QPoint(200, 200)))
-        size= self.settings.value("size", QtCore.QVariant(QtCore.QSize(400, 400)))
-        print(pos)
-        print(size)
-        if not os.path.exists("./text.txt"):
-            f = open("./text.txt","w")
-            f.close()
-        with open("./text.txt","r") as f:
-            self.str=f.readlines()
- 
-        if not os.path.exists("./ini_sticky.txt"):
-            f = open("./ini_sticky.txt","w")
-            f.close()
-        with open("./ini_sticky.txt","r") as f:
-            self.geo=f.readlines()
-        
-        # print(self.str)
-        # print(self.geo)
-        if self.geo != []:  
-            self.max_init = bool(self.geo[0].replace("\n",""))
-            self.left_init = int(self.geo[1].replace("\n",""))
-            self.top_init = int(self.geo[2].replace("\n",""))
-            # print(self.left_init)
-            # print(self.top_init)
-            self.width_init = int(self.geo[3].replace("\n",""))
-            self.height_init = int(self.geo[4].replace("\n",""))
-        else:
-            self.max_init = False
-            self.left_init = 1277
-            self.top_init = 265
-            # print(self.left_init)
-            # print(self.top_init)
-            self.width_init = 300
-            self.height_init = 335
-
-        self.ontop = True
+        self.restore_setting()
+        self.init_ui()
         self._pressed = False
         self.Direction = None
-        self.initUI()
-        # self.setMouseTracking(True)
- 
-    def initUI(self):
-        # frame
-        self.setMouseTracking(True)
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
-        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        self.setWindowOpacity(0.7)
-        self.setGeometry(self.left_init, self.top_init, self.width_init, self.height_init)
 
-        # label
-        lb2 = QLabel("Kyle's Memo", self)
-        lb2.setGeometry(QtCore.QRect(30, 0, 100, 30))
+    def init_ui(self):
+        self.set_main_window()
+        self.set_label()
+        self.set_btn()
+        self.set_text_box()
+        self.set_save_timer()
 
+    def restore_setting(self):
+        self.settings = QtCore.QSettings("config.ini",QtCore.QSettings.IniFormat)
+        self.main_window_geometry = self.settings.value("main_window/geometry",QtCore.QRect(1277, 265, 300, 335))
+        self.on_top = bool(self.settings.value("general/on_top",True))
+        self.text_word = self.settings.value("general/text_word","")
+        self.auto_save = bool(self.settings.value("general/auto_save",True))
+        self.font_size = self.settings.value("text_box/font_size","14")
+    def set_save_timer(self):
+        self.save_timer = QtCore.QTimer()
+        self.save_timer.setInterval(10000)
+        if self.auto_save == True:
+            self.save_timer.start()
+        else:
+            self.save_timer.stop()
+        self.save_timer.timeout.connect(self.save_text)
+        # self.save_timer.timeout.connect(self.print_hello)
+
+    def print_hello(self):
+        print("hello")
+
+    def set_btn(self):
         # control frame
         # close
         self.btn_close = QPushButton("", self)
-        self.btn_close.setGeometry(QtCore.QRect(self.width_init-20, 8, 15, 15))
-        self.btn_close.clicked.connect(self.cc_close)
+        self.btn_close.setGeometry(QtCore.QRect(self.main_window_geometry.width()-20, 8, 15, 15))
+        self.btn_close.clicked.connect(self.close)
         self.btn_close.setStyleSheet(
             '''QPushButton{background:#FC05AE;border-radius:7px;} QPushButton:hover{background:#BD0483;}''')
         # minimize
         self.btn_mini = QPushButton("", self)
-        self.btn_mini.setGeometry(QtCore.QRect(self.width_init-40, 8, 15, 15))
-        self.btn_mini.clicked.connect(self.cc_mini)
+        self.btn_mini.setGeometry(QtCore.QRect(self.main_window_geometry.width()-40, 8, 15, 15))
+        self.btn_mini.clicked.connect(self.showMinimized)
         self.btn_mini.setStyleSheet(
             '''QPushButton{background:#25F6AF;border-radius:7px;} QPushButton:hover{background:#1CB983;}''')
         self.btn_ontop = QPushButton("", self)
         self.btn_ontop.setGeometry(QtCore.QRect(8, 8, 15, 15))
-        self.btn_ontop.clicked.connect(self.cc_ontop)
+        self.btn_ontop.clicked.connect(self.chg_ontop)
         self.btn_ontop.setStyleSheet(
             '''QPushButton{background:#1621C6;border-radius:7px;} QPushButton:hover{background:#111995;}''')
 
-        # text board
-        self.text = QPlainTextEdit("".join(self.str),self)
-        self.text.setGeometry(QtCore.QRect(1, 30, self.width_init-2, self.height_init-31))
-        self.text.setStyleSheet('font-size:14px')
-        self.show()
+    def set_label(self):
+        lb2 = QLabel("Kyle's Memo", self)
+        lb2.setGeometry(QtCore.QRect(30, 0, 100, 30))
 
-        # if self.max_init == False:
-        self.setGeometry(self.left_init, self.top_init, self.width_init, self.height_init)
-        # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-        # else:
-            # self.showMaximized()
+    def set_main_window(self):
+        if self.on_top:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        else:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setMouseTracking(True)
+        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        self.setWindowOpacity(0.7)
+        self.setGeometry(self.main_window_geometry)
+
+    def set_text_box(self):
+        self.text_box = QPlainTextEdit("".join(self.text_word),self)
+        self.text_box.setGeometry(QtCore.QRect(1, 30, self.main_window_geometry.width()-2, self.main_window_geometry.height()-31))
+        self.text_box.setStyleSheet('font-size:'+str(self.font_size)+'px')
+        # self.text_box.cursorPositionChanged.connect(self.save_text)
+
+    def chg_ontop(self):
+        if self.on_top:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Widget)
+            self.on_top = False
+            # print(self.ontop)
+        else:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+            self.on_top = True
+            # print(self.ontop)
+        self.show()
 
     def move(self, pos):
         if self.windowState() == QtCore.Qt.WindowMaximized or self.windowState() == QtCore.Qt.WindowFullScreen:
             # 最大化或者全屏则不允许移动
             return
         super(App, self).move(pos)
-
 
     def mousePressEvent(self, event):
         """鼠标点击事件"""
@@ -127,13 +112,14 @@ class App(QWidget):
         super(App, self).mouseReleaseEvent(event)
         self._pressed = False
         self.Direction = None
-        
+
     def mouseMoveEvent(self, event):
         """鼠标移动事件"""
         super(App, self).mouseMoveEvent(event)
+        margins = 2
         pos = event.pos()
         xPos, yPos = pos.x(), pos.y()
-        wm, hm = self.width() - self.Margins, self.height() - self.Margins
+        wm, hm = self.width() - margins, self.height() - margins
         if self.isMaximized() or self.isFullScreen():
             self.Direction = None
             self.setCursor(QtCore.Qt.ArrowCursor)
@@ -143,7 +129,7 @@ class App(QWidget):
             self.moveWidget(pos)
             # print(1)
             return
-        if xPos <= self.Margins and yPos <= self.Margins:
+        if xPos <= margins and yPos <= margins:
             # 左上角
             self.Direction = LeftTop
             self.setCursor(QtCore.Qt.SizeFDiagCursor)
@@ -151,27 +137,27 @@ class App(QWidget):
             # 右下角
             self.Direction = RightBottom
             self.setCursor(QtCore.Qt.SizeFDiagCursor)
-        elif wm <= xPos and yPos <= self.Margins:
+        elif wm <= xPos and yPos <= margins:
             # 右上角
             self.Direction = RightTop
             self.setCursor(QtCore.Qt.SizeBDiagCursor)
-        elif xPos <= self.Margins and hm <= yPos:
+        elif xPos <= margins and hm <= yPos:
             # 左下角
             self.Direction = LeftBottom
             self.setCursor(QtCore.Qt.SizeBDiagCursor)
-        elif 0 <= xPos <= self.Margins and self.Margins <= yPos <= hm:
+        elif 0 <= xPos <= margins and margins <= yPos <= hm:
             # 左边
             self.Direction = Left
             self.setCursor(QtCore.Qt.SizeHorCursor)
-        elif wm <= xPos <= self.width() and self.Margins <= yPos <= hm:
+        elif wm <= xPos <= self.width() and margins <= yPos <= hm:
             # 右边
             self.Direction = Right
             self.setCursor(QtCore.Qt.SizeHorCursor)
-        elif self.Margins <= xPos <= wm and 0 <= yPos <= self.Margins:
+        elif margins <= xPos <= wm and 0 <= yPos <= margins:
             # 上面
             self.Direction = Top
             self.setCursor(QtCore.Qt.SizeVerCursor)
-        elif self.Margins <= xPos <= wm and hm <= yPos <= self.height():
+        elif margins <= xPos <= wm and hm <= yPos <= self.height():
             # 下面
             self.Direction = Bottom
             self.setCursor(QtCore.Qt.SizeVerCursor)
@@ -185,11 +171,11 @@ class App(QWidget):
         if self.Direction == None:
             moveX = x-self._mpos.x()
             moveY = y-self._mpos.y()  #计算移动了多少
-            positionX = self.frameGeometry().x() + moveX
-            positionY = self.frameGeometry().y() + moveY    #计算移动后主窗口在桌面的位置
-            self.setGeometry(QtCore.QRect(positionX, positionY, self.width_init, self.height_init))    #移动主窗口
-            self.left_init = self.geometry().left()
-            self.top_init = self.geometry().top()
+            positionX = self.geometry().x() + moveX
+            positionY = self.geometry().y() + moveY    #计算移动后主窗口在桌面的位置
+            self.setGeometry(QtCore.QRect(positionX, positionY, self.geometry().width(), self.geometry().width()))    #移动主窗口
+            self.main_window_geometry.setX(self.geometry().x())
+            self.main_window_geometry.setY(self.geometry().y())
         else:
             return
 
@@ -253,46 +239,40 @@ class App(QWidget):
                 self._mpos = pos
             else:
                 return
-        self.setGeometry(x, y, w, h)
-        self.btn_close.setGeometry(QtCore.QRect(w-20, 8, 15, 15))
-        self.btn_mini.setGeometry(QtCore.QRect(w-40, 8, 15, 15))
+        # self.setGeometry(x, y, w, h)
+        # self.btn_close.setGeometry(QtCore.QRect(w-20, 8, 15, 15))
+        # self.btn_mini.setGeometry(QtCore.QRect(w-40, 8, 15, 15))
+        # self.btn_ontop.setGeometry(QtCore.QRect(8, 8, 15, 15))
+        # self.text.setGeometry(QtCore.QRect(1, 30, w-2, h-31))
+        # self.left_init = x
+        # self.top_init = y
+        # self.width_init = w
+        # self.height_init = h
+        self.update_geometry(QtCore.QRect(x,y,w,h))
+
+    def update_geometry(self, geometry):
+        self.main_window_geometry = geometry
+        self.setGeometry(geometry)
+        self.btn_close.setGeometry(QtCore.QRect(geometry.width()-20, 8, 15, 15))
+        self.btn_mini.setGeometry(QtCore.QRect(geometry.width()-40, 8, 15, 15))
         self.btn_ontop.setGeometry(QtCore.QRect(8, 8, 15, 15))
-        self.text.setGeometry(QtCore.QRect(1, 30, w-2, h-31))
-        self.left_init = x
-        self.top_init = y
-        self.width_init = w
-        self.height_init = h
- 
-    def cc_close(self):
-        self.str = self.text.toPlainText()
-        with open("./text.txt", "w") as f:
-            f.write(self.str)
-        f.close()
- 
-        with open("./ini_sticky.txt", "w") as f:
-            s = str(self.isMaximized() or self.isFullScreen())+"\n"+str(self.left_init)+"\n"+str(self.top_init)+"\n" + str(self.geometry().width())+"\n" + str(self.geometry().height())
-            f.write(s)
-        f.close()
-        self.close()
-        self.settings.setValue("pos", QtCore.QVariant(self.pos()))
-        self.settings.setValue("size", QtCore.QVariant(self.size()))
+        self.text_box.setGeometry(QtCore.QRect(1, 30, geometry.width()-2, geometry.height()-31))
 
-    def cc_mini(self):
-        self.showMinimized()
+    def save_setting(self):
+        self.settings.setValue("main_window/geometry", self.geometry())
+        self.settings.setValue("general/on_top", self.on_top)
+        self.settings.setValue("text_box/font_size",self.font_size)
 
-    def cc_ontop(self):
-        if self.ontop:
-            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Widget)
-            self.ontop = False
-            # print(self.ontop)
-        else:
-            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
-            self.ontop = True
-            # print(self.ontop)
-        self.show()
+    def save_text(self):
+        self.settings.setValue("general/text_word", self.text_box.toPlainText())
+        # print("hello")
 
+    def closeEvent(self,event):
+        self.save_setting()
+        self.save_text()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
+    ex.show()
     sys.exit(app.exec_())
